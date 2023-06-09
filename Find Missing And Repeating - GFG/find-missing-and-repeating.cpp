476cedfd432baf3ba4914ac73a76ed8c7c058583
sharmayash1824 <133999336+sharmayash1824@users.noreply.github.com>
2023-06-09 18:58:29 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -9,27 +9,24 @@ public:
     vector<int> findTwoElement(vector<int> arr, int n) {
         // code here
         
-        
-        // sn = sum      s2n = sum of squares
-        sort(arr.begin(),arr.end());
-        int mis = -1;
-        int rep = -1;
-        for(int i =1;i<n;i++){
-            if(arr[i]-arr[i-1]>1){
-                mis = (arr[i]+arr[i-1])/2;
+        int a,b;
+        for(int i = 0;i<n;i++){
+            if(arr[abs(arr[i])-1]<0){
+                a= abs(arr[i]);
             }
-            else if(arr[i]-arr[i-1]==0){
-                rep = arr[i];
+            else{
+                arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
             }
         }
-        if(arr[0]!=1){
-            mis=1;
-        }
-        if(mis==-1){
-            mis = n;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                b = i+1;
+                break;
+            }
         }
         
-        return {rep,mis};
+        return {a,b};
     }
 };
 
