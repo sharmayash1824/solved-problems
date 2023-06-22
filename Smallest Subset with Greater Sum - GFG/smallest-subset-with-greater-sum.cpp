bbbd88d15453c14a2470f41785d314805b63fdb2
sharmayash1824 <133999336+sharmayash1824@users.noreply.github.com>
2023-06-22 18:28:10 +0530

    Added solution - LeetHub

---
+++ Smallest Subset with Greater Sum - GFG/smallest-subset-with-greater-sum.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int minSubset(vector<int> &Arr,int N){
+        sort(Arr.begin(),Arr.end());
+        long long int sum  = 0;
+        for(int i=0;i<N;i++){
+            sum+=Arr[i];
+        }
+        long long int t = 0;
+        int ans = 0;
+        for(int i = N-1;i>=0;i--){
+            t+=Arr[i];
+            ans++;
+            if(sum-t<t){
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> Arr(N);
+        for(int i=0;i<N;i++){
+            cin>>Arr[i];
+        }
+        Solution ob;
+        cout<<ob.minSubset(Arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
