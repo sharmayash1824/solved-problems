df6288abcfa7ed34b7cad64e113c6c792102b387
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-06-25 17:51:59 +0530

    Added solution - LeetHub

---
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int maximumPath(int N, vector<vector<int>> Matrix)
+    {
+        // code here
+        for(int i=1;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(j<N-1 and j>0){
+                    Matrix[i][j] += max({Matrix[i-1][j],Matrix[i-1][j-1],Matrix[i-1][j+1]});
+                }
+                else if(j>0){
+                    Matrix[i][j] += max(Matrix[i-1][j],Matrix[i-1][j-1]);
+                }
+                else if(j<N-1){
+                    Matrix[i][j] += max(Matrix[i-1][j],Matrix[i-1][j+1]);
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<N;i++){
+            ans = max(ans,Matrix[N-1][i]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<vector<int>> Matrix(N, vector<int>(N, 0));
+        for(int i = 0;i < N*N;i++)
+            cin>>Matrix[(i/N)][i%N];
+        
+        Solution ob;
+        cout<<ob.maximumPath(N, Matrix)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
