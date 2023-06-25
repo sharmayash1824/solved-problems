d3588af869541ba3289bf0ddb69d798195d23434
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-06-25 17:28:31 +0530

    Added solution - LeetHub

--- Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
@@ -12,19 +12,19 @@ public:
     int maxSquare(int n, int m, vector<vector<int>> mat){
         // code here
         int ans = 0;
-        for(int i=0;i<mat.size();i++){
+        for(int i=0;i<n;i++){
             if(mat[i][0]==1){
                 ans =1;
             }
         }
-        for(int i=0;i<mat[0].size();i++){
+        for(int i=0;i<m;i++){
             if(mat[0][i]==1){
                 ans =1;
             }
         }
         
-        for(int i=1;i<mat.size();++i){
-            for(int j=1;j<mat[i].size();++j){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(mat[i][j]==0){
                     continue;
                 }
