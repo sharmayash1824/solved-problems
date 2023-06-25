d0f28cf2406f332897028cf8937c4d44002af8cb
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-06-25 17:26:14 +0530

    Added solution - LeetHub

---
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        // code here
+        int ans = 0;
+        for(int i=0;i<mat.size();i++){
+            if(mat[i][0]==1){
+                ans =1;
+            }
+        }
+        for(int i=0;i<mat[0].size();i++){
+            if(mat[0][i]==1){
+                ans =1;
+            }
+        }
+        
+        for(int i=1;i<mat.size();++i){
+            for(int j=1;j<mat[i].size();++j){
+                if(mat[i][j]==0){
+                    continue;
+                }
+                else{
+                    mat[i][j] =1 + min(mat[i-1][j-1],min(mat[i-1][j],mat[i][j-1]));
+                    ans = max(ans,mat[i][j]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i = 0;i < n*m;i++)
+            cin>>mat[i/m][i%m];
+        
+        Solution ob;
+        cout<<ob.maxSquare(n, m, mat)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
