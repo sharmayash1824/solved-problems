4d7bc2d8c44b0388690f6e040aa9d50a9f867513
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-07-17 16:01:32 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -8,61 +8,57 @@ class Solution{
   public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
-    // Function to count inversions in the array.
-    long long merge(long long arr[], int low, int mid, int high) {
-        int left = low;
-        int right = mid + 1;
-        
-        long long inversionCount = 0;
-        
-        vector<long long> temp;
-        
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right] ) {
-                temp.push_back(arr[left]);
-                left++;
+    // Function to count inversions in the array
+    long long merge(long long N,long long arr[],long long start,long long mid,long long end){
+        long long temp[N];
+        long long int i = start;
+        long long int j = mid+1;
+        long long count = 0;
+        long long k = start;
+        while(i<=mid && j<=end){
+            if(arr[i]<=arr[j]){
+                temp[k] = arr[i];
+                k++;
+                i++;
             }
-            else {
-                temp.push_back(arr[right]);
-                inversionCount += (mid - left + 1);
-                right++;
+            else{
+                temp[k] = arr[j];
+                count += (mid-i+1);
+                j++;
+                k++;
+                
             }
         }
-    
-        while (left <= mid) {
-            temp.push_back(arr[left]);
-            left++;
-        }
-    
-        while (right <= high) {
-            temp.push_back(arr[right]);
-            right++;   
-        }
-    
-        // copy to the original array
-        for (int i=low; i<= high; i++) {
-            arr[i] = temp[i - low];
-        };
         
-        return inversionCount;
-    }
-    
-    long long mergeSort(long long arr[], int low, int high) {
-        if (low >= high) return 0;
-        long long int cnt = 0;
-        int mid = low + ( (high - low) >> 1 ) ;
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=end){
+            temp[k++]=arr[j++];
+        }
         
-        cnt += mergeSort(arr, low, mid);
-        cnt += mergeSort(arr, mid + 1, high);
-        cnt += merge(arr, low, mid, high);
-        return cnt;
+        for(long long t=start;t<=end;t++){
+            arr[t] = temp[t];
+        }
+    //   for(long long t=mid+1;t<=end;t++){
+    //         arr[t] = temp[t];
+    //     }
+        return count;
     }
-    
-    
-  public:
+    long long mergeSort(long long N,long long arr[],long long int start,long long int end){
+        long long count = 0;
+        if(start<end){
+            long long int mid = (start+end)/2;
+            count +=mergeSort(N,arr,start,mid);
+            count +=mergeSort(N,arr,mid+1,end);
+            count +=merge(N,arr,start,mid,end);
+        }
+        return count;
+    }
     long long int inversionCount(long long arr[], long long N)
     {
-        return mergeSort(arr, 0, N-1);
+        // Your Code 
+        return mergeSort(N,arr,0,N-1);
     }
 
 };
