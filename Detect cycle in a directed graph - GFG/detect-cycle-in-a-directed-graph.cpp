156be002a5978523ecdc77e16e9d435129eafb44
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-08-15 18:28:43 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -8,39 +8,32 @@ class Solution {
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
+        int indegree[V]={0};
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
         
-        // NOW BY KAHN'S ALGORITHM
-	   int indegree[V]={0};
-	   for(int i=0;i<V;i++){
-	       for(auto it:adj[i]){
-	           indegree[it]++;
-	       }
-	   }
-	   queue<int> q;
-	   for(int i=0;i<V;i++){
-	       if(indegree[i]==0){
-	           q.push(i);
-	       }
-	   }
-	   
-	   //O(v+e)
-	   vector<int> topo;
-	   while(!q.empty()){
-	       int node = q.front();
-	       q.pop();
-	       topo.push_back(node);
-	       for(auto it:adj[node]){
-	           indegree[it]--;
-	           if(indegree[it]==0) q.push(it);
-	       }
-	       
-	   }
-	   
-	   if(topo.size()<V){
-	       return true;
-	   }
-	   
-	   return false;
+        return ans.size()!=V;
     }
 };
 
