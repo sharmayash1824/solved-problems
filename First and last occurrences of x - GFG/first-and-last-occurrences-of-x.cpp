835e1076802365f8ee0886af6f3e2e03e04ef800
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-09-04 18:17:04 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -3,44 +3,46 @@
 using namespace std;
 
 // } Driver Code Ends
-
-int solve(int start,int end,int x,int arr[],bool &lower){
-    int low = start;
-    int high = end;
-    int ans = -1;
-    while(low<=high){
-        int mid = (low + high)/2;
-        if(arr[mid]<x){
-            low = mid+1;
-        }
-        else if(arr[mid]>x){
-            high = mid - 1;
-        }
-        else{
-            ans = mid;
-            if(lower) high = mid - 1;
-            else low = mid+1;
-            
-        }
-    }
-    
-    return ans;
-}
-vector<int> find(int arr[], int n , int x )
+class Solution
 {
-    // code here
-    vector<int> ans;
-    bool lower = true;
-    int first = solve(0,n-1,x,arr,lower);
-    ans.push_back(first);
-    lower=!lower;
-    int last = solve(0,n-1,x,arr,lower);
-    ans.push_back(last);
-    
-    return ans;
-    
-    
-}
+    public:
+    int helper(int low,int high,int x,int arr[],bool flag){
+        
+        int ans = -1;
+        while(low<=high){
+            int mid = (high+low)/2;
+            if(arr[mid]>x){
+                high=mid-1;
+            }
+            else if(arr[mid]<x){
+                low=mid+1;
+            }
+            else{
+                ans = mid;
+                if(flag){
+                    high = mid-1;
+                }
+                else{
+                    low=mid+1;    
+                }
+            }
+        }
+        return ans;
+    }
+    vector<int> find(int arr[], int n , int x )
+    {
+        // code here
+        bool first = true;
+        vector<int> ans;
+        ans.push_back(helper(0,n-1,x,arr,first));
+        first=!first;
+        ans.push_back(helper(0,n-1,x,arr,first));
+        
+        return ans;
+        
+        
+    }
+};
 
 //{ Driver Code Starts.
 
@@ -56,7 +58,8 @@ int main()
         for(i=0;i<n;i++)
         cin>>arr[i];
         vector<int> ans;
-        ans=find(arr,n,x);
+        Solution ob;
+        ans=ob.find(arr,n,x);
         cout<<ans[0]<<" "<<ans[1]<<endl;
     }
     return 0;
