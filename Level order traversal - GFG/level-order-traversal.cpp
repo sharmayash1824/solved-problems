535dc097aba6d73e1993adc8efcaeee3d7d377ae
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-12-07 08:41:33 +0530

    Added solution - LeetHub

--- Level order traversal - GFG/level-order-traversal.cpp
+++ Level order traversal - GFG/level-order-traversal.cpp
@@ -46,21 +46,21 @@ class Solution
     {
       //Your code here
       vector<int> ans;
-      if(node==NULL) return ans;
-        queue<Node*> q;
-        q.push(node);
-        while(!q.empty()){
-            int s = q.size();
-            for(int i=0;i<s;i++){
-                Node* temp = q.front();
-                q.pop();
-                ans.push_back(temp->data);
-                if(temp->left) q.push(temp->left);
-                if(temp->right) q.push(temp->right);
-            }
-        }
-        
-        return ans;
+      if(!node) return ans;
+      queue<Node*> q;
+      q.push(node);
+      while(!q.empty()){
+          int s = q.size();
+          for(int i=0;i<s;i++){
+              Node* temp = q.front();
+              q.pop();
+              ans.push_back(temp->data);
+              if(temp->left) q.push(temp->left); 
+              if(temp->right) q.push(temp->right); 
+          }
+      }
+      
+      return ans;
     }
 };
 
