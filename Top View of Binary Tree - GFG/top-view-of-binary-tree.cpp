c428ff02ce6922159b0184776e628250f0134b23
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-12-07 12:54:08 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
@@ -110,9 +110,7 @@ class Solution
         map<int,int> mp;
         
         queue<pair<Node*,int>> q;
-        
         q.push({root,0});
-        
         while(!q.empty()){
             int s = q.size();
             for(int i=0;i<s;i++){
@@ -122,7 +120,7 @@ class Solution
                 
                 if(mp.find(line)==mp.end()){
                     mp[line]=node->data;
-                } 
+                }
                 
                 if(node->left) q.push({node->left,line-1});
                 if(node->right) q.push({node->right,line+1});
@@ -134,6 +132,8 @@ class Solution
         }
         
         return ans;
+        
+        
     }
 
 };
