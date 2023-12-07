d4da5bf02e8baf85a83381c87f9278a1d042fd6b
Yash-Sharma <133999336+sharmayash1824@users.noreply.github.com>
2023-12-07 12:31:54 +0530

    Added solution - LeetHub

--- Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
+++ Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
@@ -46,16 +46,17 @@ class Solution
        // Your Code here
        vector<int> ans;
    if(!root) return ans;
+   
    queue<Node*> q;
    q.push(root);
    while(!q.empty()){
        int s = q.size();
        for(int i=0;i<s;i++){
-           Node* node = q.front();
-           q.pop();
-           if(i==s-1) ans.push_back(node->data);
-           if(node->left) q.push(node->left);
-           if(node->right) q.push(node->right);
+            Node* node = q.front();
+            q.pop();
+            if(i==s-1) ans.push_back(node->data);
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
        }
    }
    
